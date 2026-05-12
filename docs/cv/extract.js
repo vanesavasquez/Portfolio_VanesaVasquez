bfbe53d86ea9ee278bae4f050a2d57436421c59e
Vanesa Kanneman <vanesaledesma90@gmail.com>
2026-05-11 22:05:08 -0300

    Remove docs folder from repository - add to gitignore

--- docs/cv/extract.js
+++
@@ -1,8 +0,0 @@
-const pdfModule = require('pdf-parse');
-console.log('Type of pdfModule:', typeof pdfModule);
-console.log('Keys:', Object.keys(pdfModule));
-if (typeof pdfModule === 'function') {
-  console.log('It is a function');
-} else if (pdfModule.default) {
-  console.log('Has default:', typeof pdfModule.default);
-}
